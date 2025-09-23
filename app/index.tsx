853a816f13a4bdab52daaea5fe98b7b1ac53b356
shryl <sherylbetonio.work@gmail.com>
2025-09-23 23:15:30 +0800

    completed setup, added assets

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
-
+import './global.css';
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
